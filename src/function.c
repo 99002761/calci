52891185b08d0abbcc34c309f7530ac5cd3a0a25
99002761 <72496204+99002761@users.noreply.github.com>
2020-10-09 16:30:07 +0530

    Update function.c

--- src/function.c
+++ src/function.c
@@ -1,102 +1,71 @@
-
 #include<function.h>
 #include<stdio.h>
 #include<math.h>
 
-void addition()
+int add(int input1,int input2)
 {
-    int n, total=0, k=0, number;
-    printf("\nEnter the number of elements you want to add:");
-   
-    scanf("%d",&n);
-    printf("Please enter %d numbers one by one: \n",n);
-    while(k<n)
-    {
-        scanf("%d",&number);
-        total=total+number;
-        k=k+1;
+    int result = input1+input2;
+    return result;
+}
+
+int subtract(int input1,int input2)
+{
+    int result = input1-input2;
+    return result;
+}
+
+
+int multiply(int input1,int input2)
+{
+    int result = input1*input2;
+    return result;
+}
+
+int divide(int input1,int input2)
+{
+    int result = input1/input2;
+    return result;
+}
+
+double squareroot(double triginput)
+{
+    double result = sqrt(triginput);
+    return result;
+}
+
+int modulus(int input1,int input2)
+{
+    int result = input1 % input2;
+    return result;
+}
+
+int fact(int input1)
+{
+    int i,fact =1;
+    if (input1 < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= input1; ++i) {
+            fact *= i;
+        }
+        return fact;
     }
-    printf("Sum of %d numbers = %d \n",n,total);
 }
- 
-void subtraction()
+
+double cosine(double triginput)
 {
-    int a, b, c = 0;
-    printf("\nPlease enter first number  : ");
-    scanf("%d", &a);
-    printf("Please enter second number : ");
-    scanf("%d", &b);
-    c = a - b;
-    printf("\n%d - %d = %d\n", a, b, c);
+    double result = cos(triginput);
+    return result;
+    
 }
- 
-void multiplication()
+double sine(double triginput)
 {
-    int a, b, mul=0;
-    printf("\nPlease enter first numb   : ");
-    scanf("%d", &a);
-    printf("Please enter second number: ");
-    scanf("%d", &b);
-    mul=a*b;
-    printf("\nMultiplication of entered numbers = %d\n",mul);
+    double result = sin(triginput);
+    return result;
+    
 }
- 
-void division()
+int power(int input1,int input2)
 {
-    int a, b, d=0;
-    printf("\nPlease enter first number  : ");
-    scanf("%d", &a);
-    printf("Please enter second number : ");
-    scanf("%d", &b);
-    d=a/b;
-    printf("\nDivision of entered numbers=%d\n",d);
-}
- 
-void modulus()
-{
-    int a, b, d=0;
-    printf("\nPlease enter first number   : ");
-    scanf("%d", &a);
-    printf("Please enter second number  : ");
-    scanf("%d", &b);
-    d=a%b;
-    printf("\nModulus of entered numbers = %d\n",d);
-}
- 
-void power()
-{
-    double a,num, p;
-    printf("\nEnter two numbers to find the power \n");
-    printf("number: ");
-    scanf("%lf",&a);
- 
-    printf("power : ");
-    scanf("%lf",&num);
- 
-    p=pow(a,num);
- 
-    printf("\n%lf to the power %lf = %lf \n",a,num,p);
-}
- 
-int factorial()
-{
-    int i,fact=1,num;
- 
-    printf("\nEnter a number to find factorial : ");
-    scanf("%d",&num);
- 
-    if (num<0)
-    {
-        printf("\nPlease enter a positive number to");
-        printf(" find factorial and try again. \n");
-        printf("\nFactorial can't be found for negative");
-        printf(" values. It can be only positive or 0  \n");
-        return 1;
-    }              
- 
-    for(i=1;i<=num;i++)
-    fact=fact*i;
-    printf("\n");
-    printf("Factorial of entered number %d is:%d\n",num,fact);
-    return 0;
+    int result = pow(input1,input2);
+    return result;
 }
